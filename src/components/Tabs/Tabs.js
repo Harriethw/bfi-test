9325c98b289a4507d8f1a058bf69b090bd09a53f
harriethorobinworley <harriethw@gmail.com>
2020-07-11 11:10:50 +0100

    show active tab content

--- src/components/Tabs/Tabs.js
+++ src/components/Tabs/Tabs.js
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function Tabs(props) {
 
     const { data } = props;
 
-    const TabHeaders = () => {
-        return data.map((article, index) => {
-            const { tabTitle} = article;
-            return (<button key={index}>{tabTitle}</button>)
-        })
+    const [activeTab, setActiveTab] = useState(0);
+
+    const selectTab = (event, index) => {
+        event.preventDefault();
+        setActiveTab(index);
     }
 
-    return (<><TabHeaders /></>)
+    const TabHeaders = () => data.map((section, index) => {
+        const { tabTitle } = section;
+        return (<button onClick={(e) => selectTab(e, index)} key={index} data-testid={`tab-${index}`}>{tabTitle}</button>)
+    })
+
+    const TabContent = () => data.map((section, sectionIndex) => {
+        const { results } = section;
+        const content = results.map((result, index) => {
+            return (<ul key={index} style={{display: sectionIndex === activeTab ? 'block' : 'none' }}>{index+1}. <a href={result.url}>{result.title}</a></ul>)
+        })
+        return content;
+    })
+
+    return (<><TabHeaders /><TabContent/></>)
 };
--- src/components/Tabs/Tabs.test.js
+++ src/components/Tabs/Tabs.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import each from 'jest-each';
 import Tabs from './Tabs';
 import * as mockData from './mockData/mockArticles.json'
@@ -16,6 +17,25 @@ describe('Tabs behaviour', () => {
         let heading = screen.getByText(article.tabTitle);
         expect(heading).toBeInTheDocument();
     });
+
+    it('renders only the first tab links at start', () => {
+        renderPage();
+        const firstLink = mockData.default[0].results[0].title;
+        const hiddenLink = mockData.default[1].results[0].title
+        expect(screen.getByText(firstLink)).toBeInTheDocument();
+        expect(screen.getByText(hiddenLink)).not.toBeVisible();
+    })
+
+    it('renders correct links when tab clicked', () => {
+        renderPage();
+        const firstLink = mockData.default[0].results[0].title;
+        const hiddenLink = mockData.default[1].results[0].title;
+        expect(screen.getByTestId('tab-1')).toBeInTheDocument();
+
+        userEvent.click(screen.getByTestId('tab-1'));
+        expect(screen.getByText(hiddenLink)).toBeVisible();
+        expect(screen.getByText(firstLink)).not.toBeVisible();
+    })
 
 })
 
